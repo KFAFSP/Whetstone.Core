b6e3ad855281b45b545b35c70dc79352ee4a6c08
Karl F. A. Friebel <karl.friebel@friebelnet.de>
2019-01-11 19:44:44 +0100

    Added Queue.

---
+++ Whetstone.Core.Tasks/Queue.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using JetBrains.Annotations;
+
+using Whetstone.Core.Contracts;
+
+namespace Whetstone.Core.Tasks
+{
+    /// <summary>
+    /// Represents a waiting queue.
+    /// </summary>
+    /// <remarks>
+    /// Essentially implements a mutex lock.
+    /// </remarks>
+    [PublicAPI]
+    public sealed class Queue : Disposable, ISynchronizationSource
+    {
+        sealed class Handle : SynchronizationHandle
+        {
+            public static Handle CreateHead()
+            {
+                var handle = new Handle();
+                handle.Dispose();
+                return handle;
+            }
+
+            #region SynchronizationHandle overrides
+            /// <inheritdoc />
+            protected override void Release()
+            {
+                Released.TryEnd();
+            }
+            #endregion
+
+            public void Fault()
+            {
+                Released.Dispose();
+                Dispose();
+            }
+
+            public Era Released { get; } = new Era();
+        }
+
+        [NotNull]
+        Handle FHead;
+        [NotNull]
+        Handle FTail;
+
+        /// <summary>
+        /// Create a new <see cref="Queue"/>.
+        /// </summary>
+        public Queue()
+        {
+            FHead = Handle.CreateHead();
+            FTail = FHead;
+        }
+
+        #region Disposable overrides
+        /// <inheritdoc />
+        protected override void Dispose(bool ADisposing)
+        {
+            if (ADisposing)
+            {
+                FHead.Fault();
+            }
+
+            base.Dispose(ADisposing);
+        }
+        #endregion
+
+        #region ISynchronizationSource
+        /// <inheritdoc />
+        public async Task<SynchronizationHandle> WaitAsync(CancellationToken ACancel)
+        {
+            ThrowIfDisposed();
+            ACancel.ThrowIfCancellationRequested();
+
+            var handle = new Handle();
+            var predecessor = Interlocked.Exchange(ref FTail, handle);
+
+            try
+            {
+                await predecessor.Released.WaitAsync(ACancel);
+            }
+            catch (OperationCanceledException)
+            {
+                // NOTE: This continuation runs synchronously at the parent task.
+#pragma warning disable 4014
+                predecessor.Released.WaitAsync()
+                    .ContinueWith(
+                        A => handle.Dispose(),
+                        TaskContinuationOptions.ExecuteSynchronously
+                    );
+#pragma warning restore 4014
+            }
+            catch (ObjectDisposedException)
+            {
+                handle.Fault();
+                throw;
+            }
+
+            FHead = handle;
+            return handle;
+        }
+        #endregion
+
+        /// <summary>
+        /// Get a value indicating whether the queue is empty.
+        /// </summary>
+        public bool IsEmpty => FHead.IsReleased;
+    }
+}
---
+++ Whetstone.Core.Tests/Tasks/Queue.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+using NUnit.Framework;
+
+namespace Whetstone.Core.Tasks
+{
+    [TestFixture]
+    [Category("Threading")]
+    [TestOf(typeof(Queue))]
+    public sealed class QueueTests
+    {
+        [TearDown]
+        public void Teardown()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+
+        [Test]
+        public void Dispose_FaultsAwaiters()
+        {
+            var queue = new Queue();
+
+            var awaiters = new[]
+            {
+                queue.WaitAsync(),
+                queue.WaitAsync(),
+                queue.WaitAsync()
+            };
+
+            queue.Dispose();
+
+            using (TaskAssert.Completed(awaiters.First())) { }
+
+            foreach (var awaiter in awaiters.Skip(1))
+            {
+                TaskAssert.Faulted<ObjectDisposedException>(awaiter);
+            }
+        }
+
+        [Test]
+        public void WaitAsync_Disposed_ThrowsObjectDisposedException()
+        {
+            var queue = new Queue();
+            queue.Dispose();
+
+            TaskAssert.Faulted<ObjectDisposedException>(queue.WaitAsync());
+        }
+
+        [Test]
+        public void WaitAsync_Empty_ReturnsCompletedTask()
+        {
+            using (var queue = new Queue())
+            {
+                using (TaskAssert.Completed(queue.WaitAsync())) { }
+            }
+        }
+
+        [Test]
+        public void WaitAsync_NotEmpty_WaitsForTurn()
+        {
+            using (var queue = new Queue())
+            {
+                var t1 = queue.WaitAsync();
+                var t2 = queue.WaitAsync();
+                var t3 = queue.WaitAsync();
+
+                using (TaskAssert.Completed(t1))
+                {
+                    TaskAssert.DoesNotEnd(t2);
+                    TaskAssert.DoesNotEnd(t3);
+                }
+
+                using (TaskAssert.Completed(t2))
+                {
+                    TaskAssert.DoesNotEnd(t3);
+                }
+
+                using (TaskAssert.Completed(t3))
+                {
+                    Assert.That(!queue.IsEmpty);
+                }
+
+                Assert.That(queue.IsEmpty);
+            }
+        }
+
+        [Test]
+        public void WaitAsync_NotEmpty_CanCedeTurn()
+        {
+            using (var cts = new CancellationTokenSource())
+            using (var queue = new Queue())
+            {
+                var t1 = queue.WaitAsync();
+                var t2 = queue.WaitAsync(cts.Token);
+                var t3 = queue.WaitAsync();
+
+                using (TaskAssert.Completed(t1))
+                {
+                    TaskAssert.DoesNotEnd(t2);
+                    TaskAssert.DoesNotEnd(t3);
+
+                    cts.Cancel();
+                    TaskAssert.DoesNotEnd(t3);
+                }
+
+                using (TaskAssert.Completed(t3))
+                {
+                    Assert.That(!queue.IsEmpty);
+                }
+
+                Assert.That(queue.IsEmpty);
+            }
+        }
+    }
+}
