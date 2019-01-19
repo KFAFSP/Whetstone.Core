728c67d3bc85ae07df5f23b1c558bb972bb13b6f
Karl F. A. Friebel <karl.friebel@friebelnet.de>
2019-01-19 14:38:35 +0100

    Added StringExtensions.Excerpt.

---
+++ Whetstone.Core.Tests/Text/StringExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+
+using JetBrains.Annotations;
+
+using NUnit.Framework;
+
+using Whetstone.Core.Contracts;
+
+namespace Whetstone.Core.Text
+{
+    [TestFixture]
+    [Category("Text")]
+    [TestOf(typeof(StringExtensions))]
+    public sealed class StringExtensionsTests
+    {
+        [TestCase(-1)]
+        [TestCase(0)]
+        public void Excerpt_NonPositiveMaxLength_ThrowsArgumentOutOfRangeException(int AMaxLength)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => _ = "".Excerpt(AMaxLength)
+            );
+        }
+
+        [TestCaseSource(nameof(ExcerptShortStringTestCases))]
+        public string Excerpt_ShortString_ReturnsQuotedString(string AInput)
+            => AInput.Excerpt(10);
+
+        [TestCaseSource(nameof(ExcerptLongStringTestCases))]
+        public string Excerpt_LongString_ReturnsExcerptQuote(string AInput)
+            => AInput.Excerpt(10);
+
+        [UsedImplicitly]
+        static IEnumerable ExcerptShortStringTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(null)
+                    .Returns("null");
+                yield return new TestCaseData("")
+                    .Returns("\"\"");
+                yield return new TestCaseData("null")
+                    .Returns("\"null\"");
+                yield return new TestCaseData("0123456789")
+                    .Returns("\"0123456789\"");
+            }
+        }
+
+        [UsedImplicitly]
+        static IEnumerable ExcerptLongStringTestCases
+        {
+            get
+            {
+                yield return new TestCaseData("00000000001")
+                    .Returns("\"0000000000\" + 1 char(s)");
+                yield return new TestCaseData(string.Concat("0".Repeat(100)))
+                    .Returns("\"0000000000\" + 90 char(s)");
+            }
+        }
+    }
+}
---
+++ Whetstone.Core.Text/StringExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using JetBrains.Annotations;
+
+using Whetstone.Core.Contracts;
+
+namespace Whetstone.Core.Text
+{
+    /// <summary>
+    /// Provides static extension methods for the <see cref="string"/> class.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Get a printable excerpt from a string.
+        /// </summary>
+        /// <param name="AString">The string.</param>
+        /// <param name="AMaxLength">The maximum excerpt length.</param>
+        /// <returns>
+        /// An excerpt of the string that contains up to the first <paramref name="AMaxLength"/>
+        /// characters in <paramref name="AString"/>, optionally indicating the number of remaining
+        /// characters.
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="AMaxLength"/> is 0 or less.
+        /// </exception>
+        [Pure]
+        [NotNull]
+        public static string Excerpt([CanBeNull] this string AString, int AMaxLength = 30)
+        {
+            Require.Positive(AMaxLength, nameof(AMaxLength));
+
+            if (AString == null)
+            {
+                return "null";
+            }
+
+            if (AString.Length > AMaxLength)
+            {
+                var excerpt = AString
+                    .Substring(0, AMaxLength)
+                    .Replace("\"", "\\\"");
+
+                return $"\"{excerpt}\" + {AString.Length - AMaxLength} char(s)";
+            }
+
+            var str = AString
+                .Replace("\"", "\\\"");
+
+            return $"\"{str}\"";
+        }
+    }
+}
