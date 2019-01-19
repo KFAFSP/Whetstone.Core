8e2f5df4a7462c5475e834a66f0eeffe15db8e13
Karl F. A. Friebel <karl.friebel@friebelnet.de>
2019-01-19 18:13:13 +0100

    Added TextLocationAdapter.

---
+++ Whetstone.Core.Tests/Text/TextLocationAdapter.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+using JetBrains.Annotations;
+
+using NUnit.Framework;
+
+using Whetstone.Core.Contracts;
+
+namespace Whetstone.Core.Text
+{
+    [TestFixture]
+    [Category("Text")]
+    [TestOf(typeof(TextLocationAdapter))]
+    public sealed class TextLocationAdapterTests
+    {
+        [Test]
+        public void Constructor_InitialLocation()
+        {
+            using (var tla = new TextLocationAdapter(TextReader.Null))
+            {
+                Assert.That(tla.Line, Is.EqualTo(1));
+                Assert.That(tla.Character, Is.EqualTo(1));
+            }
+        }
+
+        [TestCase(-1, 1)]
+        [TestCase(0, 1)]
+        [TestCase(1, -1)]
+        [TestCase(1, 0)]
+        public void Synchronize_NotPostive_ThrowsArgumentOutOfRangeException(int ALine, int AChar)
+        {
+            using (var tla = new TextLocationAdapter(TextReader.Null))
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(
+                    () => tla.Synchronize(ALine, AChar)
+                );
+            }
+        }
+
+        [Test]
+        public void Synchronize_Positive_SetsLocation()
+        {
+            using (var tla = new TextLocationAdapter(TextReader.Null))
+            {
+                tla.Synchronize(10, 10);
+
+                Assert.That(tla.Line, Is.EqualTo(10));
+                Assert.That(tla.Character, Is.EqualTo(10));
+            }
+        }
+
+        [TestCase("", ExpectedResult = -1)]
+        [TestCase("a", ExpectedResult = 'a')]
+        public int Peek_Delegates(string AInput)
+        {
+            using (var tla = new TextLocationAdapter(new StringReader(AInput)))
+            {
+                var peek = tla.Peek();
+
+                Assert.That(tla.Line, Is.EqualTo(1));
+                Assert.That(tla.Character, Is.EqualTo(1));
+
+                return peek;
+            }
+        }
+
+        [TestCaseSource(nameof(ReadOnceTestCases))]
+        public (int, long, long) Read_Once_DelegatesAndProcesses(string AInput)
+        {
+            using (var tla = new TextLocationAdapter(new StringReader(AInput)))
+            {
+                var read = tla.Read();
+
+                return (read, tla.Line, tla.Character);
+            }
+        }
+
+        [TestCaseSource(nameof(ReadTestCases))]
+        public (long, long) Read_DelegatesAndProcesses(string AInput)
+        {
+            using (var tla = new TextLocationAdapter(new StringReader(AInput)))
+            {
+                var buffer = new char[AInput.Length];
+                var read = tla.Read(buffer, 0, buffer.Length);
+
+                Assert.That(read, Is.EqualTo(buffer.Length));
+                Assert.That(string.Concat(buffer), Is.EqualTo(AInput));
+
+                return (tla.Line, tla.Character);
+            }
+        }
+
+        [TestCaseSource(nameof(ReadTestCases))]
+        public (long, long) ReadBlock_DelegatesAndProcesses(string AInput)
+        {
+            using (var tla = new TextLocationAdapter(new StringReader(AInput)))
+            {
+                var buffer = new char[AInput.Length];
+                var read = tla.ReadBlock(buffer, 0, buffer.Length);
+
+                Assert.That(read, Is.EqualTo(buffer.Length));
+                Assert.That(string.Concat(buffer), Is.EqualTo(AInput));
+
+                return (tla.Line, tla.Character);
+            }
+        }
+
+        [Test]
+        public void ReadLine_DelegatesAndProcesses()
+        {
+            using (var tla = new TextLocationAdapter(new StringReader("abc\r\ndef")))
+            {
+                var line = tla.ReadLine();
+
+                Assert.That(line, Is.EqualTo("abc"));
+                Assert.That(tla.Line, Is.EqualTo(2));
+                Assert.That(tla.Character, Is.EqualTo(1));
+            }
+        }
+
+        [TestCaseSource(nameof(ReadTestCases))]
+        public (long, long) ReadToEnd_DelegatesAndProcesses(string AInput)
+        {
+            using (var tla = new TextLocationAdapter(new StringReader(AInput)))
+            {
+                var read = tla.ReadToEnd();
+
+                Assert.That(read, Is.EqualTo(AInput));
+
+                return (tla.Line, tla.Character);
+            }
+        }
+
+        [Test]
+        public void Dispose_LeaveOpen_LeavesOpen()
+        {
+            using (var outer = new StringReader("abc"))
+            {
+                using (var tla = new TextLocationAdapter(outer, true)) { }
+
+                outer.Read();
+            }
+        }
+
+        [Test]
+        public void Dispose_NotLeaveOpen_Closes()
+        {
+            using (var outer = new StringReader("abc"))
+            {
+                using (var tla = new TextLocationAdapter(outer, false)) { }
+
+                Assert.Throws<ObjectDisposedException>(
+                    () => _ = outer.Read()
+                );
+            }
+        }
+
+        [Test]
+        public void Close_ClosesBase()
+        {
+            using (var tla = new TextLocationAdapter(new StringReader("abc")))
+            {
+                tla.Close();
+
+                Assert.Throws<ObjectDisposedException>(
+                    () => _ = tla.Base.Read()
+                );
+            }
+        }
+
+        [UsedImplicitly]
+        static IEnumerable ReadOnceTestCases
+        {
+            get
+            {
+                yield return new TestCaseData("")
+                    .Returns((-1, 1L, 1L));
+                yield return new TestCaseData("a")
+                    .Returns(((int)'a', 1L, 2L));
+                yield return new TestCaseData("\n")
+                    .Returns(((int)'\n', 2L, 1L));
+            }
+        }
+
+        [UsedImplicitly]
+        static IEnumerable ReadTestCases
+        {
+            get
+            {
+                yield return new TestCaseData("")
+                    .Returns((1L, 1L));
+                yield return new TestCaseData("abc")
+                    .Returns((1L, 4L));
+                yield return new TestCaseData("a\na\r")
+                    .Returns((2L, 3L));
+                yield return new TestCaseData("\naaaa\naaa\n")
+                    .Returns((4L, 1L));
+            }
+        }
+    }
+}
---
+++ Whetstone.Core.Text/TextLocationAdapter.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.IO;
+
+using JetBrains.Annotations;
+
+using Whetstone.Core.Contracts;
+
+namespace Whetstone.Core.Text
+{
+    /// <summary>
+    /// Adapter for the <see cref="TextReader"/> class that tracks the current location.
+    /// </summary>
+    [PublicAPI]
+    public sealed class TextLocationAdapter : TextReader
+    {
+        const char C_LineFeed = '\n';
+
+        /// <summary>
+        /// Create a new <see cref="TextLocationAdapter"/>.
+        /// </summary>
+        /// <param name="AReader">The underlying <see cref="TextReader"/>.</param>
+        /// <param name="ALeaveOpen">
+        /// If <see langword="true"/>, <paramref name="AReader"/> will not be closed when this
+        /// adapter is disposed.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="AReader"/> is <see langword="null"/>.
+        /// </exception>
+        public TextLocationAdapter([NotNull] TextReader AReader, bool ALeaveOpen = false)
+        {
+            Require.NotNull(AReader, nameof(AReader));
+
+            Base = AReader;
+            LeaveOpen = ALeaveOpen;
+
+            Line = 1;
+            Character = 1;
+        }
+
+        void UpdatePos([NotNull] char[] ABuffer, int AIndex, int ACount)
+        {
+            Ensure.NotNull(ABuffer, nameof(ABuffer));
+            Ensure.IndexRange(
+                ABuffer,
+                AIndex,
+                nameof(AIndex),
+                ACount,
+                nameof(ACount)
+            );
+
+            // Count all and find the last occurrence of C_LineFeed.
+            var last = -1;
+            for (var I = 0; I < ABuffer.Length; ++I)
+            {
+                if (ABuffer[I] == C_LineFeed)
+                {
+                    Line++;
+                    last = I;
+                }
+            }
+
+            if (last == -1)
+            {
+                // No C_LineFeed was found, so add the characters.
+                Character += ACount;
+            }
+            else
+            {
+                // Character location depends on distance to last C_LineFeed.
+                Character = ACount - last;
+            }
+        }
+        void UdpatePos([NotNull] string ABuffer)
+        {
+            Ensure.NotNull(ABuffer, nameof(ABuffer));
+
+            var offset = ABuffer.IndexOf(C_LineFeed);
+            if (offset == -1)
+            {
+                // No C_LineFeed was found, so add the characters.
+                Character += ABuffer.Length;
+                return;
+            }
+
+            // Count all and find the last occurrence of C_LineFeed.
+            while (true)
+            {
+                Line++;
+
+                var find = ABuffer.IndexOf(C_LineFeed, offset + 1);
+                if (find == -1)
+                {
+                    break;
+                }
+
+                offset = find;
+            }
+
+            // Character location depends on distance to last C_LineFeed.
+            Character = ABuffer.Length - offset;
+        }
+
+        /// <summary>
+        /// Set the location manually.
+        /// </summary>
+        /// <param name="ALine">The line index.</param>
+        /// <param name="ACharacter">The character index.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="ALine"/> is 0 or less.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="ACharacter"/> is 0 or less.
+        /// </exception>
+        public void Synchronize(int ALine, int ACharacter)
+        {
+            Require.Positive(ALine, nameof(ALine));
+            Require.Positive(ACharacter, nameof(ACharacter));
+
+            Line = ALine;
+            Character = ACharacter;
+        }
+
+        #region TextReader overrides
+        /// <inheritdoc />
+        /// <exception cref="IOException">An I/O error occurred.</exception>
+        /// <exception cref="ObjectDisposedException"><see cref="Base"/> is disposed.</exception>
+        public override int Peek() => Base.Peek();
+
+        /// <inheritdoc />
+        /// <exception cref="IOException">An I/O error occurred.</exception>
+        /// <exception cref="ObjectDisposedException"><see cref="Base"/> is disposed.</exception>
+        public override int Read()
+        {
+            var read = Base.Read();
+            switch (read)
+            {
+                case C_LineFeed:
+                    // Line feed increases line index and resets character index.
+                    Line++;
+                    Character = 1;
+                    break;
+
+                case -1:
+                    // End of stream propagates.
+                    return -1;
+
+                default:
+                    // Any other character increases character index.
+                    Character++;
+                    return read;
+            }
+
+            return read;
+        }
+        /// <inheritdoc />
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="ABuffer"/> is <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="AIndex"/> or <paramref name="ACount"/> are negative.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Length of <paramref name="ABuffer"/> minus <paramref name="AIndex"/> is less than
+        /// <paramref name="ACount"/>.
+        /// </exception>
+        /// <exception cref="IOException">An I/O error occurred.</exception>
+        /// <exception cref="ObjectDisposedException"><see cref="Base"/> is disposed.</exception>
+        public override int Read(char[] ABuffer, int AIndex, int ACount)
+        {
+            var read = Base.Read(ABuffer, AIndex, ACount);
+            // Scan the buffer to update the location.
+            UpdatePos(ABuffer, AIndex, read);
+
+            return read;
+        }
+
+        /// <inheritdoc />
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="ABuffer"/> is <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="AIndex"/> or <paramref name="ACount"/> are negative.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Length of <paramref name="ABuffer"/> minus <paramref name="AIndex"/> is less than
+        /// <paramref name="ACount"/>.
+        /// </exception>
+        /// <exception cref="IOException">An I/O error occurred.</exception>
+        /// <exception cref="ObjectDisposedException"><see cref="Base"/> is disposed.</exception>
+        public override int ReadBlock(char[] ABuffer, int AIndex, int ACount)
+        {
+            var read = Base.ReadBlock(ABuffer, AIndex, ACount);
+            // Scan the buffer to update the location.
+            UpdatePos(ABuffer, AIndex, read);
+
+            return read;
+        }
+
+        /// <inheritdoc />
+        /// <exception cref="IOException">An I/O error occurred.</exception>
+        /// <exception cref="OutOfMemoryException">
+        /// Not enough memory for the line buffer.
+        /// </exception>
+        /// <exception cref="ObjectDisposedException"><see cref="Base"/> is disposed.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// The next line is longer than <see cref="int.MaxValue"/>.
+        /// </exception>
+        public override string ReadLine()
+        {
+            var read = Base.ReadLine();
+            // The semantics of this method enforces this new location.
+            Line++;
+            Character = 1;
+
+            return read;
+        }
+
+        /// <inheritdoc />
+        /// <exception cref="IOException">An I/O error occurred.</exception>
+        /// <exception cref="OutOfMemoryException">Not enough memory for the buffer.</exception>
+        /// <exception cref="ObjectDisposedException"><see cref="Base"/> is disposed.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// The remainder is longer than <see cref="int.MaxValue"/>.
+        /// </exception>
+        public override string ReadToEnd()
+        {
+            var read = Base.ReadToEnd();
+            // Scan the buffer to update the location.
+            UdpatePos(read);
+
+            return read;
+        }
+
+        /// <inheritdoc />
+        /// <remarks>
+        /// Will dispose <see cref="Base"/> if <see cref="LeaveOpen"/> is <see langword="false"/>.
+        /// </remarks>
+        protected override void Dispose(bool ADisposing)
+        {
+            // Only dispose Base when LeaveOpen is false.
+            if (ADisposing && !LeaveOpen)
+            {
+                Base.Dispose();
+            }
+        }
+
+        /// <inheritdoc />
+        /// <remarks>
+        /// Will always close <see cref="Base"/>, regardless of <see cref="LeaveOpen"/>.
+        /// </remarks>
+        public override void Close()
+        {
+            Base.Close();
+        }
+        #endregion
+
+        /// <summary>
+        /// Get the underlying <see cref="TextReader"/>.
+        /// </summary>
+        [NotNull]
+        public TextReader Base { get; }
+        /// <summary>
+        /// Get a value indicating whether <see cref="Base"/> will be left open on
+        /// <see cref="Dispose"/>.
+        /// </summary>
+        public bool LeaveOpen { get; }
+
+        /// <summary>
+        /// Get the current line location.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// The indicated location is always the location of the next character that will be read,
+        /// which is the one that can be <see cref="Peek"/>ed.
+        /// </para>
+        /// <para>
+        /// Line indices start at 1.
+        /// </para>
+        /// </remarks>
+        public long Line { get; private set; }
+        /// <summary>
+        /// Get the current character location.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// The indicated location is always the location of the next character that will be read,
+        /// which is the one that can be <see cref="Peek"/>ed.
+        /// </para>
+        /// <para>
+        /// Character indices start at 1 for each line.
+        /// </para>
+        /// </remarks>
+        public long Character { get; private set; }
+    }
+}
