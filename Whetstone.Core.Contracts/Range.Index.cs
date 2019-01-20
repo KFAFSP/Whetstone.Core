95cce2aee66ecce8008c8341ae953e77312e1e5e
Karl F. A. Friebel <karl.friebel@friebelnet.de>
2019-01-20 21:58:05 +0100

    Added Range.Index.

---
+++ Whetstone.Core.Contracts/Range.Index.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using JetBrains.Annotations;
+
+namespace Whetstone.Core.Contracts
+{
+    public partial class Range
+    {
+        /// <summary>
+        /// Initialize a [0, count) <see cref="int"/> index <see cref="Range{T}"/>.
+        /// </summary>
+        /// <param name="ACount">The exclusive upper limit.</param>
+        /// <returns>The [0, <paramref name="ACount"/>) <see cref="Range{T}"/>.</returns>
+        [Pure]
+        public static Range<int> Indices(this int ACount)
+            => Of(0, true, ACount, false);
+
+        /// <summary>
+        /// Initialize an index range for characters in a <see cref="string"/>.
+        /// </summary>
+        /// <param name="AString">The <see cref="string"/>.</param>
+        /// <returns>The [0, <paramref name="AString"/> length) <see cref="Range{T}"/>.</returns>
+        /// <remarks>
+        /// If <paramref name="AString"/> is <see langword="null"/>, it is assumed to be empty.
+        /// </remarks>
+        [Pure]
+        public static Range<int> Indices([CanBeNull] this string AString)
+            => Indices(AString?.Length ?? 0);
+
+        /// <summary>
+        /// Initialize an index range for items in a 1D <see cref="Array"/>.
+        /// </summary>
+        /// <typeparam name="T">The array item type.</typeparam>
+        /// <param name="AArray">The 1D <see cref="Array"/>.</param>
+        /// <returns>The [0, <paramref name="AArray"/> length) <see cref="Range{T}"/>.</returns>
+        /// <exception cref="OverflowException">
+        /// The length of <paramref name="AArray"/> exceeds <see cref="int.MaxValue"/>.
+        /// </exception>
+        /// <remarks>
+        /// <para>
+        /// If <paramref name="AArray"/> is <see langword="null"/>, it is assumed to be empty.
+        /// </para>
+        /// <para>
+        /// Consider using <see cref="LongIndices{T}(T[])"/> if <paramref name="AArray"/> is
+        /// expected to have lengths exceeding <see cref="int.MaxValue"/>.
+        /// </para>
+        /// </remarks>
+        [Pure]
+        public static Range<int> Indices<T>([CanBeNull] this T[] AArray)
+            => Indices(AArray?.Length ?? 0);
+
+        /// <summary>
+        /// Initialize an index range for items in a <see cref="ICollection"/>.
+        /// </summary>
+        /// <param name="ACollection">The <see cref="ICollection"/>.</param>
+        /// <returns>
+        /// The [0, <paramref name="ACollection"/> length) <see cref="Range{T}"/>.
+        /// </returns>
+        /// <remarks>
+        /// If <paramref name="ACollection"/> is <see langword="null"/>, it is assumed to be empty.
+        /// </remarks>
+        [Pure]
+        public static Range<int> Indices([CanBeNull] this ICollection ACollection)
+            => Indices(ACollection?.Count ?? 0);
+
+        /// <summary>
+        /// Initialize an index range for items in a <see cref="ICollection{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">The collection item type.</typeparam>
+        /// <param name="ACollection">The <see cref="ICollection{T}"/>.</param>
+        /// <returns>
+        /// The [0, <paramref name="ACollection"/> length) <see cref="Range{T}"/>.
+        /// </returns>
+        /// <remarks>
+        /// If <paramref name="ACollection"/> is <see langword="null"/>, it is assumed to be empty.
+        /// </remarks>
+        [Pure]
+        public static Range<int> Indices<T>([CanBeNull] this ICollection<T> ACollection)
+            => Indices(ACollection?.Count ?? 0);
+
+        /// <summary>
+        /// Initialize a [0, count) <see cref="long"/> index <see cref="Range{T}"/>.
+        /// </summary>
+        /// <param name="ACount">The exclusive upper limit.</param>
+        /// <returns>The [0, <paramref name="ACount"/>) <see cref="Range{T}"/>.</returns>
+        [Pure]
+        public static Range<long> LongIndices(this long ACount)
+            => Of(0L, true, ACount, false);
+
+        /// <summary>
+        /// Initialize an index range for items in a 1D <see cref="Array"/>.
+        /// </summary>
+        /// <typeparam name="T">The array item type.</typeparam>
+        /// <param name="AArray">The 1D <see cref="Array"/>.</param>
+        /// <returns>The [0, <paramref name="AArray"/> length) <see cref="Range{T}"/>.</returns>
+        /// <remarks>
+        /// <para>
+        /// If <paramref name="AArray"/> is <see langword="null"/>, it is assumed to be empty.
+        /// </para>
+        /// <para>
+        /// Consider using <see cref="Indices{T}(T[])"/> if <paramref name="AArray"/> is sure to
+        /// have lengths not exceeding <see cref="int.MaxValue"/>.
+        /// </para>
+        /// </remarks>
+        [Pure]
+        public static Range<long> LongIndices<T>([CanBeNull] this T[] AArray)
+            => LongIndices(AArray?.LongLength ?? 0L);
+    }
+}
---
+++ Whetstone.Core.Tests/Contracts/Range.Index.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using JetBrains.Annotations;
+
+using NUnit.Framework;
+
+namespace Whetstone.Core.Contracts
+{
+    public partial class RangeTests
+    {
+        static readonly List<int> _FEmptyList = new List<int>();
+        static readonly List<int> _FShortList = new List<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        [TestCaseSource(nameof(IndicesInt32TestCases))]
+        public Range<int> Indices_Int32_ReturnsIndexRange(int AValue)
+            => AValue.Indices();
+
+        [UsedImplicitly]
+        static IEnumerable IndicesInt32TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(0)
+                    .Returns(Range.Of(0, true, 0, false));
+                yield return new TestCaseData(10)
+                    .Returns(Range.Of(0, true, 10, false));
+            }
+        }
+
+        [TestCaseSource(nameof(IndicesStringTestCases))]
+        public Range<int> Indices_String_ReturnsIndexRange(string AValue)
+            => AValue.Indices();
+
+        [UsedImplicitly]
+        static IEnumerable IndicesStringTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(null)
+                    .Returns(Range.Of(0, true, 0, false));
+                yield return new TestCaseData("")
+                    .Returns(Range.Of(0, true, 0, false));
+                yield return new TestCaseData("abc")
+                    .Returns(Range.Of(0, true, 3, false));
+            }
+        }
+
+        [TestCaseSource(nameof(IndicesArrayTestCases))]
+        public Range<int> Indices_Array_ReturnsIndexRange(int[] AValue)
+            => AValue.Indices();
+
+        [UsedImplicitly]
+        static IEnumerable IndicesArrayTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(null)
+                    .Returns(Range.Of(0, true, 0, false));
+                yield return new TestCaseData(new int[0])
+                    .Returns(Range.Of(0, true, 0, false));
+                yield return new TestCaseData(new int[10])
+                    .Returns(Range.Of(0, true, 10, false));
+            }
+        }
+
+        [TestCaseSource(nameof(IndicesCollectionTestCases))]
+        public Range<int> Indices_Collection_ReturnsIndexRange(ICollection AValue)
+            => AValue.Indices();
+
+        [UsedImplicitly]
+        static IEnumerable IndicesCollectionTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(null)
+                    .Returns(Range.Of(0, true, 0, false));
+                yield return new TestCaseData(_FEmptyList)
+                    .Returns(Range.Of(0, true, 0, false));
+                yield return new TestCaseData(_FShortList)
+                    .Returns(Range.Of(0, true, _FShortList.Count, false));
+            }
+        }
+
+        [TestCaseSource(nameof(IndicesCollectionTestCases))]
+        public Range<int> Indices_CollectionT_ReturnsIndexRange(ICollection<int> AValue)
+            => AValue.Indices();
+
+        [TestCaseSource(nameof(LongIndicesLongTestCases))]
+        public Range<long> LongIndices_Long_ReturnsIndexRange(long AValue)
+            => AValue.LongIndices();
+
+        [UsedImplicitly]
+        static IEnumerable LongIndicesLongTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(0L)
+                    .Returns(Range.Of(0L, true, 0L, false));
+                yield return new TestCaseData(10L)
+                    .Returns(Range.Of(0L, true, 10L, false));
+            }
+        }
+
+        [TestCaseSource(nameof(LongIndicesArrayTestCases))]
+        public Range<long> LongIndices_Array_ReturnsIndexRange(int[] AValue)
+            => AValue.LongIndices();
+
+        [UsedImplicitly]
+        static IEnumerable LongIndicesArrayTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(null)
+                    .Returns(Range.Of(0L, true, 0L, false));
+                yield return new TestCaseData(new int[0])
+                    .Returns(Range.Of(0L, true, 0L, false));
+                yield return new TestCaseData(new int[10])
+                    .Returns(Range.Of(0L, true, 10L, false));
+            }
+        }
+    }
+}
