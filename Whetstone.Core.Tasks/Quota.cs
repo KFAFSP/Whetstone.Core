b28cfa1a945cd38af256b56cfa9706180b00ee45
Karl F. A. Friebel <karl.friebel@friebelnet.de>
2019-01-22 22:42:56 +0100

    Added Quota.

---
+++ Whetstone.Core.Tasks/Quota.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using JetBrains.Annotations;
+
+using Whetstone.Core.Contracts;
+
+namespace Whetstone.Core.Tasks
+{
+    /// <summary>
+    /// Represents a quota that limits entry to a section.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Essentially implements a semaphore.
+    /// </para>
+    /// </remarks>
+    [PublicAPI]
+    public sealed class Quota : Disposable, ISynchronizationSource
+    {
+        sealed class Handle : SynchronizationHandle
+        {
+            [NotNull]
+            readonly Quota FParent;
+
+            public Handle([NotNull] Quota AParent)
+            {
+                Ensure.NotNull(AParent, nameof(AParent));
+
+                FParent = AParent;
+            }
+
+            #region SynchronizationHandle overrides
+            /// <inheritdoc />
+            protected override void Release()
+            {
+                FParent.Release();
+            }
+            #endregion
+        }
+
+        [NotNull]
+        readonly Turnstile FWaiting = new Turnstile();
+        long FBalance;
+        long FActive;
+
+        /// <summary>
+        /// Create a new <see cref="Quota"/>.
+        /// </summary>
+        /// <param name="AQuota">The quota.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="AQuota"/> is 0 or less.
+        /// </exception>
+        public Quota(long AQuota)
+        {
+            Require.Positive(AQuota, nameof(AQuota));
+
+            FBalance = AQuota;
+        }
+
+        #region Disposable overrides
+        /// <inheritdoc />
+        protected override void Dispose(bool ADisposing)
+        {
+            if (ADisposing)
+            {
+                // Fault all remaining awaiters.
+                FWaiting.Dispose();
+            }
+
+            base.Dispose(ADisposing);
+        }
+        #endregion
+
+        /// <summary>
+        /// Decrement the active count and increment the balance.
+        /// </summary>
+        void Release()
+        {
+            // Exit the section.
+            Interlocked.Decrement(ref FActive);
+
+            // Increase the balance.
+            if (Interlocked.Increment(ref FBalance) <= 0)
+            {
+                // Release the next reservation.
+                FWaiting.Turn();
+            }
+        }
+
+        #region ISynchronizationSource
+        /// <inheritdoc />
+        /// <exception cref="OperationCanceledException">
+        /// <paramref name="ACancel"/> was canceled.
+        /// </exception>
+        /// <exception cref="ObjectDisposedException">This instance is disposed.</exception>
+        public async Task<SynchronizationHandle> WaitAsync(CancellationToken ACancel)
+        {
+            ThrowIfDisposed();
+            ACancel.ThrowIfCancellationRequested();
+
+            // Decrement the balance.
+            if (Interlocked.Decrement(ref FBalance) < 0)
+            {
+                // Reservation was made, await our turn.
+                try
+                {
+                    await FWaiting.WaitAsync(ACancel);
+                }
+                catch (OperationCanceledException)
+                {
+                    // Waiting was canceled.
+                    // Increment the balance again to release the reservation.
+                    // NOTE: Reference is never null.
+                    // ReSharper disable once ExceptionNotDocumented
+                    Interlocked.Increment(ref FBalance);
+                    throw;
+                }
+            }
+
+            // Enter the section.
+            // NOTE: Reference is never null.
+            // ReSharper disable once ExceptionNotDocumented
+            Interlocked.Increment(ref FActive);
+            return new Handle(this);
+        }
+        #endregion
+
+        /// <summary>
+        /// Get the current quota balance.
+        /// </summary>
+        /// <remarks>
+        /// The balance is the difference between available quota and made reservations. If it is
+        /// negative, some reservation is still waiting.
+        /// </remarks>
+        public long Balance => FBalance;
+        /// <summary>
+        /// Get the number of tasks currently in the monitored section.
+        /// </summary>
+        public long Active => FActive;
+    }
+}
---
+++ Whetstone.Core.Tests/Tasks/Quota.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+using NUnit.Framework;
+
+// ReSharper disable ExceptionNotDocumented
+// ReSharper disable AssignmentIsFullyDiscarded
+
+namespace Whetstone.Core.Tasks
+{
+    [TestFixture]
+    [Category("Tasks")]
+    [TestOf(typeof(Quota))]
+    public sealed class QuotaTests
+    {
+        [TearDown]
+        public void TearDown()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+
+        [TestCase(0L)]
+        [TestCase(-1L)]
+        public void Constructor_NotPositive_ThrowsArgumentOutOfRangeException(long AValue)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => _ = new Quota(AValue)
+            );
+        }
+
+        [Test]
+        public void Constructor_Positive_HasQuota()
+        {
+            using (var quota = new Quota(3L))
+            {
+                Assert.That(quota.Balance, Is.EqualTo(3L));
+            }
+        }
+
+        [Test]
+        public void Dispose_FaultsAwaiters()
+        {
+            var quota = new Quota(1L);
+
+            using (var cts = new CancellationTokenSource())
+            {
+                var awaiters = new[]
+                {
+                    quota.WaitAsync(),
+                    quota.WaitAsync(cts.Token),
+                    quota.WaitAsync(),
+                    quota.WaitAsync()
+                };
+
+                cts.Cancel();
+                quota.Dispose();
+
+                using (TaskAssert.Completed(awaiters[0])) { }
+                TaskAssert.Cancelled(awaiters[1]);
+
+                foreach (var awaiter in awaiters.Skip(2))
+                {
+                    TaskAssert.Faulted<ObjectDisposedException>(awaiter);
+                }
+            }
+        }
+
+        [Test]
+        public void WaitAsync_Disposed_ThrowsObjectDisposedException()
+        {
+            var quota = new Quota(1L);
+            quota.Dispose();
+
+            TaskAssert.Faulted<ObjectDisposedException>(quota.WaitAsync());
+        }
+
+        [Test]
+        public void WaitAsync_Available_ReturnsCompletedTask()
+        {
+            using (var quota = new Quota(3L))
+            {
+                var release = new IDisposable[3];
+
+                for (var I = 0; I <= 2; ++I)
+                {
+                    Assert.That(quota.Active, Is.EqualTo(I));
+                    release[I] = TaskAssert.Completed(quota.WaitAsync());
+                }
+
+                for (var I = 2; I >= 0; --I)
+                {
+                    release[I].Dispose();
+                    Assert.That(quota.Active, Is.EqualTo(I));
+                }
+            }
+        }
+
+        [Test]
+        public void WaitAsync_Exceeded_WaitsForTurn()
+        {
+            using (var quota = new Quota(1L))
+            {
+                var t1 = quota.WaitAsync();
+                var t2 = quota.WaitAsync();
+                var t3 = quota.WaitAsync();
+
+                Assert.That(quota.Balance, Is.EqualTo(-2L));
+
+                using (TaskAssert.Completed(t1))
+                {
+                    TaskAssert.DoesNotEnd(t2);
+                    TaskAssert.DoesNotEnd(t3);
+                }
+
+                Assert.That(quota.Balance, Is.EqualTo(-1L));
+
+                using (TaskAssert.Completed(t2))
+                {
+                    TaskAssert.DoesNotEnd(t3);
+                }
+
+                Assert.That(quota.Balance, Is.EqualTo(0L));
+
+                using (TaskAssert.Completed(t3)) { }
+
+                Assert.That(quota.Balance, Is.EqualTo(1L));
+            }
+        }
+
+        [Test]
+        public void WaitAsync_Exceeded_CanCedeTurn()
+        {
+            using (var cts = new CancellationTokenSource())
+            using (var quota = new Quota(1L))
+            {
+                var t1 = quota.WaitAsync();
+                var t2 = quota.WaitAsync(cts.Token);
+                var t3 = quota.WaitAsync();
+
+                Assert.That(quota.Balance, Is.EqualTo(-2L));
+
+                cts.Cancel();
+
+                Assert.That(quota.Balance, Is.EqualTo(-1L));
+
+                using (TaskAssert.Completed(t1))
+                {
+                    TaskAssert.Cancelled(t2);
+                    TaskAssert.DoesNotEnd(t3);
+                }
+
+                Assert.That(quota.Balance, Is.EqualTo(0L));
+
+                using (TaskAssert.Completed(t3)) { }
+
+                Assert.That(quota.Balance, Is.EqualTo(1L));
+            }
+        }
+    }
+}
