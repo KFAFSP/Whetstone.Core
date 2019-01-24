7bde60066bd63970fdbdf463a0b6a3cc36fa666e
Karl F. A. Friebel <karl.friebel@friebelnet.de>
2019-01-24 15:09:26 +0100

    Fixed Dispose.

--- Whetstone.Core.Contracts/Dispose.cs
+++ Whetstone.Core.Contracts/Dispose.cs
@@ -7,6 +7,17 @@ using JetBrains.Annotations;
 namespace Whetstone.Core.Contracts
 {
     /// <summary>
+    /// Common delegate for an action that implements <see cref="IDisposable.Dispose()"/>.
+    /// </summary>
+    /// <typeparam name="T">The object type.</typeparam>
+    /// <param name="AInstance">The ref-reference of the instance.</param>
+    /// <remarks>
+    /// Because of value type <typeparamref name="T"/>s, <paramref name="AInstance"/> must be
+    /// passed by ref-reference.
+    /// </remarks>
+    public delegate void DisposeAction<T>(ref T AInstance);
+
+    /// <summary>
     /// Provides helpers for dealing with objects that implement the disposable pattern.
     /// </summary>
     [PublicAPI]
@@ -21,52 +32,72 @@ namespace Whetstone.Core.Contracts
         /// The <see cref="MethodInfo"/> for the preffered implementor, or <see langword="null"/> if
         /// none found.
         /// </returns>
+        /// <exception cref="TargetInvocationException">
+        /// <paramref name="AType"/> ran a static initializer that threw an exception.
+        /// </exception>
         [CanBeNull]
         internal static MethodInfo GetImpl([NotNull] Type AType)
         {
             Ensure.NotNull(AType, nameof(AType));
 
-            // Priority 1: Interface implementation takes precedence.
-            if (AType.GetInterfaces().Contains(typeof(IDisposable)))
+            if (!AType.GetInterfaces().Contains(typeof(IDisposable)))
             {
-                var disposableImpl = AType.GetInterfaceMap(typeof(IDisposable));
-                return disposableImpl.TargetMethods[0];
+                // Only interface implementations are allowed.
+                return null;
             }
 
-            // Priority 2: Public Dispose() instance method.
-            return AType.GetMethod(
-                @"Dispose",
-                BindingFlags.Public | BindingFlags.Instance,
-                Type.DefaultBinder,
-                Type.EmptyTypes,
-                null
-            );
+            // Get the implementing method.
+            var disposableImpl = AType.GetInterfaceMap(typeof(IDisposable));
+            return disposableImpl.TargetMethods[0];
         }
 
         /// <summary>
-        /// Get the dispose <see cref="Action{T}"/> for instances of the target type.
+        /// Get the dispose <see cref="DisposeAction{T}"/> for instances of the target type.
         /// </summary>
         /// <typeparam name="T">The target type.</typeparam>
         /// <returns>
         /// An open delegate to the implementation of the Disposable pattern (if any); otherwise
         /// <see langword="null"/>.
         /// </returns>
+        /// <exception cref="TargetInvocationException">
+        /// <typeparamref name="T"/> ran a static initializer that threw an exception.
+        /// </exception>
         [CanBeNull]
-        internal static Action<T> GetAction<T>()
+        internal static DisposeAction<T> GetAction<T>()
         {
             var type = typeof(T);
             var impl = GetImpl(type);
 
-            return impl != null
-                ? (Action<T>)Delegate.CreateDelegate(typeof(Action<T>), impl)
-                : null;
+            if (impl == null)
+            {
+                // Not implemented.
+                return null;
+            }
+
+            if (type.IsValueType)
+            {
+                // For reference types, instance methods bind to ref-references.
+                // NOTE: Does not throw.
+                // ReSharper disable once ExceptionNotDocumented
+                return (DisposeAction<T>)
+                    Delegate.CreateDelegate(typeof(DisposeAction<T>), impl);
+            }
+
+            // For reference types, instance methods bind to object references.
+            // NOTE: Does not throw.
+            // ReSharper disable once ExceptionNotDocumented
+            var action = (Action<T>)
+                Delegate.CreateDelegate(typeof(Action<T>), impl);
+
+            // Wrap in another delegate that drops the ref-reference.
+            return (ref T X) => action(X);
         }
 
         /// <summary>
         /// Safely dispose of any generic object.
         /// </summary>
         /// <typeparam name="T">The object type.</typeparam>
-        /// <param name="AObject">The object.</param>
+        /// <param name="AObject">A ref-reference to the object.</param>
         /// <remarks>
         /// <para>
         /// If <paramref name="AObject"/> is <see langword="null"/>, nothing will be peformed.
@@ -77,15 +108,17 @@ namespace Whetstone.Core.Contracts
         /// be performed.
         /// </para>
         /// </remarks>
-        public static void Of<T>(T AObject)
+        /// <exception cref="Exception">The dispose action threw an exception.</exception>
+        public static void Of<T>(ref T AObject)
         {
             var type = typeof(T);
             if (!type.IsValueType && ReferenceEquals(AObject, null))
             {
+                // Null object references are not disposed.
                 return;
             }
 
-            Dispose<T>.Action?.Invoke(AObject);
+            Dispose<T>.Action?.Invoke(ref AObject);
         }
     }
 
@@ -97,12 +130,13 @@ namespace Whetstone.Core.Contracts
     public static class Dispose<T>
     {
         [NotNull]
-        readonly static Lazy<Action<T>> _FAction = new Lazy<Action<T>>(Dispose.GetAction<T>);
+        static readonly Lazy<DisposeAction<T>> _FAction =
+            new Lazy<DisposeAction<T>>(Dispose.GetAction<T>);
 
         /// <summary>
-        /// Get the dispose <see cref="Action{T}"/> for type <typeparamref name="T"/>.
+        /// Get the dispose <see cref="DisposeAction{T}"/> for type <typeparamref name="T"/>.
         /// </summary>
         [CanBeNull]
-        public static Action<T> Action => _FAction.Value;
+        public static DisposeAction<T> Action => _FAction.Value;
     }
 }
---
+++ Whetstone.Core.Tests/Contracts/Dispose.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+using JetBrains.Annotations;
+
+using NUnit.Framework;
+
+// ReSharper disable ExceptionNotDocumented
+
+namespace Whetstone.Core.Contracts
+{
+    [TestFixture]
+    [Category("Contracts")]
+    [TestOf(typeof(Dispose))]
+    public sealed class DisposeTests
+    {
+        [Test]
+        public void Of_NullReference_DoesNothing()
+        {
+            DisposeTTests.ClassImpl obj = null;
+            Dispose.Of(ref obj);
+        }
+
+        [Test]
+        public void Of_DisposableReference_Disposes()
+        {
+            var disposable = new DisposeTTests.ClassImpl();
+            Dispose.Of(ref disposable);
+            Assert.That(disposable.IsDisposed);
+        }
+
+        [Test]
+        public void Of_DisposableValue_Disposes()
+        {
+            var disposable = new DisposeTTests.StructImpl();
+            Dispose.Of(ref disposable);
+            Assert.That(disposable.IsDisposed);
+        }
+    }
+
+    [TestFixture]
+    [Category("Contracts")]
+    [TestOf(typeof(Dispose<>))]
+    public sealed class DisposeTTests
+    {
+        [UsedImplicitly]
+        public sealed class ClassImpl : IDisposable
+        {
+            #region IDisposable
+            void IDisposable.Dispose()
+            {
+                IsDisposed = true;
+            }
+            #endregion
+
+            [UsedImplicitly]
+            public void Dispose()
+            {
+                // It's a trap!
+            }
+
+            public bool IsDisposed { get; private set; }
+        }
+
+        [UsedImplicitly]
+        public struct StructImpl : IDisposable
+        {
+            public void Dispose()
+            {
+                IsDisposed = true;
+            }
+
+            public bool IsDisposed { get; private set; }
+        }
+
+        [UsedImplicitly]
+        public sealed class NoImpl { }
+
+        [Test]
+        public void Action_InterfaceImpl_ImplMethodAction()
+        {
+            var obj = new ClassImpl();
+            var action = Dispose<ClassImpl>.Action;
+
+            Assert.That(action, Is.Not.Null);
+            action(ref obj);
+            Assert.That(obj.IsDisposed);
+        }
+
+        [Test]
+        public void Action_StructImpl_MethodAction()
+        {
+            var obj = new StructImpl();
+            var action = Dispose<StructImpl>.Action;
+
+            Assert.That(action, Is.Not.Null);
+            action(ref obj);
+            Assert.That(obj.IsDisposed);
+        }
+
+        [Test]
+        public void Action_NoImpl_Null()
+        {
+            var action = Dispose<NoImpl>.Action;
+
+            Assert.That(action, Is.Null);
+        }
+    }
+}
